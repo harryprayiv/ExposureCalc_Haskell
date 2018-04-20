4d0eea09507e1f17637856d469b6a19398a53115
harryprayiv <harryprayiv@gmail.com>
2018-04-20 19:19:12 -0400

    moar fixes to evCalc

--- exposure.hs
+++ exposure.hs
@@ -11,7 +11,7 @@ iso i = i / 100
 ev :: Double -> Double
 ev e = logBase e 2
 
--- Define evCalc
+-- Define evCalc (not entirely correct yet)
 evCalc :: Double -> Double -> Double -> Double
 evCalc iso shutter stop = logBase 2((stop)/(iso*shutter))
 
@@ -21,14 +21,14 @@ ev = evCalc (stop 2.8) (iso 320.0) (shutter 48.0)
 -- Define stopCalc
 stopCalc :: Double -> Double -> Double -> Double
 stopCalc iso shutter ev = undefined
---(sqrt(((exp2(ev))*(iso*shutter))/100)*100.0)/100.0
+--in swift (sqrt(((exp2(ev))*(iso*shutter))/100)*100.0)/100.0
 
 -- Define isoCalc
 isoCalc :: Double -> Double -> Double -> Double
 isoCalc stop shutter ev = undefined
--- (100*((pow(stop, 2))) / (exp2(ev))/shutter)
+-- in swft (100*((pow(stop, 2))) / (exp2(ev))/shutter)
 
 -- Define shutterCalc
 shutterCalc :: Double -> Double -> Double -> Double
 shutterCalc stop iso ev = undefined
--- (100*(pow(stop, 2)) / (exp2(ev)))/iso
+-- in swift (100*(pow(stop, 2)) / (exp2(ev)))/iso
