064c03b90124ffbaaee975d2a5e2c5f9541c0a25
harryprayiv <harryprayiv@gmail.com>
2018-04-21 23:16:21 -0400

    more improvements

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ It's still wrong as hell but this is the first commit.  These are the formulas p
 
 I still have to translate the syntax into proper Haskell.  At the moment, it compiles but I'm not sure how correct it actually is.  
 
-Loving Haskell so far.  Amazingly mind-opening!
+Loving Haskell so far.  Amazingly mind-opening!  Now I need a better REPL so I can get real-time feedback on stuff.
--- exposure.hs
+++ exposure.hs
@@ -14,19 +14,20 @@ ev e = logBase e 2
 -- Define evCalc (not entirely correct yet)
 evCalc :: Double -> Double -> Double -> Double
 evCalc iso shutter stop = logBase 2((stop)/(iso*shutter))
-
+-- in swift: log2((100*(pow(aperture!,2)))/(iSO!*shutterTime!))
+--__________________________________________________
 --quick test of function
-ev = evCalc (stop 2.8) (iso 320.0) (shutter 48.0)
+eValue = evCalc (stop 2.8) (iso 320.0) (shutter 48.0)
 
 -- Define stopCalc
 stopCalc :: Double -> Double -> Double -> Double
 stopCalc iso shutter ev = undefined
---in swift (sqrt(((exp2(ev))*(iso*shutter))/100)*100.0)/100.0
+--in swift (sqrt(((exp2(LV!))*(iSO!*shutterTime!))/100)*100.0)/100.0
 
 -- Define isoCalc
 isoCalc :: Double -> Double -> Double -> Double
 isoCalc stop shutter ev = undefined
--- in swft (100*((pow(stop, 2))) / (exp2(ev))/shutter)
+-- in swft (100*((pow(aperture!, 2))) / (exp2(LV!))/shutterTime!)
 
 -- Define shutterCalc
 shutterCalc :: Double -> Double -> Double -> Double
