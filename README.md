568804e19cc72e62a0e074a90dbc03aef7150b6b
harryprayiv <harryprayiv@gmail.com>
2018-04-23 01:10:50 -0400

    the equations are starting to work.

--- README.md
+++ README.md
@@ -5,3 +5,8 @@ It's still wrong as hell but this is the first commit.  These are the formulas p
 I still have to translate the syntax into proper Haskell.  At the moment, it compiles but I'm not sure how correct it actually is.  
 
 Loving Haskell so far.  Amazingly mind-opening!  Now I need a better REPL so I can get real-time feedback on stuff.
+
+In swift, solve for EV: log2((100*(pow(aperture!,2)))/(iSO!*shutterTime!))
+solve for Aperture: (sqrt(((exp2(LV!))*(iSO!*shutterTime!))/100)*100.0)/100.0
+solve for ISO: (100*((pow(aperture!, 2))) / (exp2(LV!))/shutterTime!)
+solve for Shutter: shutter (100*(pow(stop, 2)) / (exp2(ev)))/iso
--- exposure.hs
+++ exposure.hs
@@ -1,35 +1,19 @@
-
-stop :: Double -> Double
-stop f = f * f
-
-shutter :: Double -> Double
-shutter s = 1 / s
-
-iso :: Double -> Double
-iso i = i / 100
-
-ev :: Double -> Double
-ev e = logBase e 2
-
--- Define evCalc (not entirely correct yet)
-evCalc :: Double -> Double -> Double -> Double
-evCalc iso shutter stop = logBase 2((stop)/(iso*shutter))
--- in swift: log2((100*(pow(aperture!,2)))/(iSO!*shutterTime!))
---__________________________________________________
---quick test of function
-eValue = evCalc (stop 2.8) (iso 320.0) (shutter 48.0)
+-- Define evCalc
+ev :: Double -> Double -> Double -> Double
+ev n i s = logBase 2 (100 * ((n ** 2) / (i * s)))
 
 -- Define stopCalc
-stopCalc :: Double -> Double -> Double -> Double
-stopCalc iso shutter ev = undefined
---in swift (sqrt(((exp2(LV!))*(iSO!*shutterTime!))/100)*100.0)/100.0
+stop :: Double -> Double -> Double -> Double
+stop e i s = (sqrt (((2 ** (e)) * (i * s))/100) * 100.0)/100.0
 
 -- Define isoCalc
-isoCalc :: Double -> Double -> Double -> Double
-isoCalc stop shutter ev = undefined
--- in swft (100*((pow(aperture!, 2))) / (exp2(LV!))/shutterTime!)
+iso :: Double -> Double -> Double -> Double
+iso n e s = (100 * (n ** 2) / (2 ** e)/ s)
 
 -- Define shutterCalc
-shutterCalc :: Double -> Double -> Double -> Double
-shutterCalc stop iso ev = undefined
--- in swift (100*(pow(stop, 2)) / (exp2(ev)))/iso
+--shutter :: Double -> Double -> Double -> Double
+--shutter n t = logBase 2 (100 * (n / ())))
+
+ev 6.531972647421807 100.0 (1/48)
+stop 11 100 (1/48)
+iso 6.531972647421807 11.0 (1/48)
