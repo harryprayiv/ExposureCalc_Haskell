ef12fc2cecafdc7f9306c080d1ccfb1aea752fdb
harryprayiv <harryprayiv@gmail.com>
2018-04-23 01:24:05 -0400

    cleaning up overused parenthesis

--- exposure.hs
+++ exposure.hs
@@ -4,11 +4,11 @@ ev n i s = logBase 2 (100 * ((n ** 2) / (i * s)))
 
 -- Define stopCalc
 stop :: Double -> Double -> Double -> Double
-stop e i s = (sqrt (((2 ** (e)) * (i * s))/100) * 100.0)/100.0
+stop e i s = (sqrt (((2 ** e) * (i * s))/100) * 100.0)/100.0
 
 -- Define isoCalc
 iso :: Double -> Double -> Double -> Double
-iso n e s = (100 * (n ** 2) / (2 ** e)/ s)
+iso n e s = 100 * (n ** 2) / (2 ** e)/ s
 
 -- Define shutterCalc
 shutter :: Double -> Double -> Double -> Double
